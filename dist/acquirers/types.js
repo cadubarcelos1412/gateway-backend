18ded1c59a4ac1ba4cf8d772a6aa14d60130f381
KissaPay <barceloscadu14@gmail.com>
2025-10-27 21:59:58 -0300

    sei la o que fazer

--- dist/acquirers/types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// src/acquirers/types.ts
-/* -------------------------------------------------------------------------- */
-/* 📦 Tipos globais usados por todos os adquirentes                           */
-/* -------------------------------------------------------------------------- */
-Object.defineProperty(exports, "__esModule", { value: true });
